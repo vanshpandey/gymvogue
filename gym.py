a9e7df9bc3517ff42a9aeaf5db40d58176a137fd
Saurabh Tiwari <129385426+Rexaintreal@users.noreply.github.com>
2024-09-26 15:59:16 +0530

    Update gym.py

--- gym.py
+++ gym.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,json
 
 app = Flask(__name__)
 
@@ -14,5 +14,21 @@ def products():
 def productdetail():
     return render_template('productdetail.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/bags')
+def dufflebags():
+    # Load the bags data from JSON file
+    with open('bags.json') as f:
+        bags = json.load(f)["bags"]
+    return render_template('dufflebags.html', bags=bags)
+
+
 if __name__ == '__main__':
-    app.run(debug=True,host='192.168.1.34')
+    app.run(debug=True, host='192.168.1.34')
